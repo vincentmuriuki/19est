e0f8d937c4bf15b6b477b7e0ffc537bd46710db8
Vincent Muriuki <vincentmuriuki42@gmail.com>
2020-04-12 22:52:49 +0300

    Removed white spaces

--- src/estimator.js
+++ src/estimator.js
@@ -12,8 +12,8 @@ import impactCalculator from './calculations';
 
 const covid19ImpactEstimator = (data) => ({
   data,
-  impact: impactCalculator({...data}, 10),
-  severeImpact: impactCalculator({...data}, 50)
+  impact: impactCalculator({ ...data }, 10),
+  severeImpact: impactCalculator({ ...data }, 50)
 });
 
 export default covid19ImpactEstimator;
