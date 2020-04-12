03b3e52071bb315d534e376f0fc286b261348ed0
Vincent Muriuki <vincentmuriuki42@gmail.com>
2020-04-12 22:50:10 +0300

    export default estimator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import { onCovid19 } from 'on-covid-19';
-import estimator from './estimator';
+// import { onCovid19 } from 'on-covid-19';
+// import estimator from './estimator';
 
-const lagosNG = {
-  reportedCases: 50,
-  population: 22000000
-};
+// const lagosNG = {
+//   reportedCases: 50,
+//   population: 22000000
+// };
 
-const results = onCovid19(lagosNG, estimator).estimateImpactAfter(30).days();
+// const results = onCovid19(lagosNG, estimator).estimateImpactAfter(30).days();
 
-console.dir(results);
+// console.dir(results);
