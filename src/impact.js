831140b77ce189ff5b2e4262a8421cb47ef409a6
Vincent Muriuki <vincentmuriuki42@gmail.com>
2020-04-13 10:27:00 +0300

    Infections projections def

--- src/impact.js
+++ src/impact.js
@@ -1,32 +1,36 @@
 import data from './data';
 // import calInfectionProjections from './infectionProjections';
 import calculateNoOfDays from './calculateDays';
-import calMoneyLost from './calMoneyLost';
+// import calMoneyLost from './calMoneyLost';
 
 const noOfDays = calculateNoOfDays(data.periodType, data.timeToElapse);
 const currentlyInfected = data.reportedCases * 10;
 const infectionsByRequestedTime = currentlyInfected * 2 ** Math.trunc(noOfDays / 3);
 
-
-const casesByRequestedTime = Math.ceil(infectionsByRequestedTime * 0.15);
-const hospitalBedsByRequestedTime = Math.ceil(
-  data.totalHospitalBeds - casesByRequestedTime
-);
-const dollarsInFlight = calMoneyLost(
-  infectionsByRequestedTime,
-  data.region.avgDailyIncomePopulation,
-  data.region.avgDailyIncomeInUSD,
-  noOfDays
-);
+// const casesByRequestedTime = Math.ceil(infectionsByRequestedTime * 0.15);
+// const hospitalBedsByRequestedTime = Math.ceil(
+//   data.totalHospitalBeds - casesByRequestedTime
+// );
+// const dollarsInFlight = calMoneyLost(
+//   infectionsByRequestedTime,
+//   data.region.avgDailyIncomePopulation,
+//   data.region.avgDailyIncomeInUSD,
+//   noOfDays
+// );
 
 const impact = {
   currentlyInfected,
-  infectionsByRequestedTime,
-  casesByRequestedTime,
-  hospitalBedsByRequestedTime,
-  casesForICUByRequestedTime: 0.05 * infectionsByRequestedTime,
-  casesForVentilatorsByRequestedTime: 0.02 * infectionsByRequestedTime,
-  dollarsInFlight
+  infectionsByRequestedTime
 };
+
+// const impact = {
+//   currentlyInfected,
+//   infectionsByRequestedTime,
+//   casesByRequestedTime,
+//   hospitalBedsByRequestedTime,
+//   casesForICUByRequestedTime: 0.05 * infectionsByRequestedTime,
+//   casesForVentilatorsByRequestedTime: 0.02 * infectionsByRequestedTime,
+//   dollarsInFlight
+// };
 
 export default impact;
